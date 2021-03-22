b61fbf4f092ffe05da4ddb6f0fe98c8bdd921a05
Ådne Karstad <aadne.karstad@gmail.com>
2021-03-22 09:45:20 +0100

    add title format and extract plotting function

--- data_analyser.py
+++ data_analyser.py
@@ -20,45 +20,32 @@ def get_column_data(column='A'):
     return data, title
 
 
+def format_title(title):
+    title_len = len(title)
+    if title_len > 50:
+        # Add half the words to two separate lines.
+        title_words = title.split()
+        num_words = len(title_words)
+        return f'{" ".join(title_words[0:num_words//2])}\n{" ".join(title_words[num_words//2:])}'
+    return title
+        
+
+
 def plot_hours_spent(*columns):
     """ Plot a bar diagram of answers related to hours spent on subject """
     for column in columns:
-        values, title = get_column_data(column)
-
-        bar_values = [values.count(i) for i in range(0,21)]
-
-        bars = np.arange(0, 21, 1)
-        
-        mean = round(sum(values) / len(values), 3)
-        plt.title(f'{title}\nGjennomsnitt: {str(mean)}')
-
-        plt.bar(bars, bar_values, align='center')
+        plt, title = plot_diagram(column, 1, 20+1)
 
         plt.xlabel('Antall timer')
-        plt.ylabel('Antall svar')
-        plt.grid(axis='y')
-
         plt.savefig(f'./../diagrams/hours/{title.replace(" ", "_").lower()}_bar.png')
         plt.close()
 
 def plot_satisfaction(*columns):
     """ Questions related to scale of 1=Ikke tilfreds to 5=Svært tilfreds """
     for column in columns:
-        values, title = get_column_data(column)
-
-        bar_values = [values.count(1), values.count(2), values.count(3), values.count(4), values.count(5)]
-
-        bars = np.arange(1, 5 + 1)
-        
-        mean = round(sum(values) / len(values), 3)
-        plt.title(f'{title}\nGjennomsnitt: {str(mean)}')
-
-        plt.bar(bars, bar_values, align='center')
+        plt, title = plot_diagram(column, 1, 5+1)
 
         plt.xlabel('1=Ikke tilfreds | 5=Svært tilfreds')
-        plt.ylabel('Antall svar')
-        plt.grid(axis='y')
-
         plt.savefig(f'./../diagrams/satisfaction/{title.replace(" ", "_").lower()}_bar.png')
         plt.close()
 
@@ -66,24 +53,28 @@ def plot_satisfaction(*columns):
 def plot_agreement(*columns):
     """ Questions related to scale of 1=Helt uenig to 5=Helt enig """
     for column in columns:
-        values, title = get_column_data(column)
-
-        bar_values = [values.count(i) for i in range(1,5+1)]
-
-        bars = np.arange(1, 5 + 1)
-        
-        mean = round(sum(values) / len(values), 3)
-        plt.title(f'{title}\nGjennomsnitt: {str(mean)}')
-
-        plt.bar(bars, bar_values, align='center')
+        plt, title = plot_diagram(column, 1, 5+1)
 
         plt.xlabel('1=Helt uenig | 5=Helt enig')
-        plt.ylabel('Antall svar')
-        plt.grid(axis='y')
-
         plt.savefig(f'./../diagrams/agreement/{title.replace(" ", "_").lower()}_bar.png')
         plt.close()
 
+
+def plot_diagram(column, x_low, x_high):
+    values, title = get_column_data(column)
+
+    bar_values = [values.count(i) for i in range(x_low, x_high)]
+    bars = np.arange(x_low, x_high)
+    
+    mean = round(sum(values) / len(values), 1)
+    plt.bar(bars, bar_values, align='center')
+
+    plt.title(f'{format_title(title)}\nGjennomsnitt: {str(mean)}')
+    plt.subplots_adjust(top=0.8)
+    plt.grid(axis='y')
+    plt.ylabel('Antall svar')
+
+    return plt, title
 
 
 if __name__ == '__main__':
