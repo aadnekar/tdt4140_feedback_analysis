f0e0844d52adcbf230f861be016a6a02994e56e1
aadnekar <aadne.karstad@gmail.com>
2021-10-10 14:34:26 +0200

    Write small readme and run linter

--- data_analyser.py
+++ data_analyser.py
@@ -9,18 +9,19 @@ from openpyxl import load_workbook
 NUMBER_OF_RESPONSES = 142
 NUMBER_OF_ROWS = NUMBER_OF_RESPONSES + 1
 
-wb = load_workbook(filename='./../results_iterasjon_2.xlsx')
-RESULTS_DIR = './../diagrams/iterasjon2'
+wb = load_workbook(filename="./../results_iterasjon_2.xlsx")
+RESULTS_DIR = "./../diagrams/iterasjon2"
 
 
 sheet = wb.active
 
-def get_column_data(column='A'):
+
+def get_column_data(column="A"):
     """
-        get all data in the specified column, returns list of data
+    get all data in the specified column, returns list of data
     """
-    title = sheet[f'{column}1'].value
-    cells = sheet[f'{column}2':f'{column}{NUMBER_OF_RESPONSES}']
+    title = sheet[f"{column}1"].value
+    cells = sheet[f"{column}2":f"{column}{NUMBER_OF_RESPONSES}"]
     data = [int(cell[0].value) for cell in cells]
 
     return data, title
@@ -34,35 +35,39 @@ def format_title(title):
         num_words = len(title_words)
         return f'{" ".join(title_words[0:num_words//2])}\n{" ".join(title_words[num_words//2:])}'
     return title
-        
 
 
 def plot_hours_spent(*columns):
-    """ Plot a bar diagram of answers related to hours spent on subject """
+    """Plot a bar diagram of answers related to hours spent on subject"""
     for column in columns:
-        plt, title = plot_diagram(column, 1, 20+1)
+        plt, title = plot_diagram(column, 1, 20 + 1)
 
-        plt.xlabel('Antall timer')
+        plt.xlabel("Antall timer")
         plt.savefig(f'{RESULTS_DIR}/hours/{title.replace(" ", "_").lower()}_bar.png')
         plt.close()
 
-def plot_satisfaction(*columns):
-    """ Questions related to scale of 1=Ikke tilfreds to 5=Svært tilfreds """
-    for column in columns:
-        plt, title = plot_diagram(column, 1, 5+1)
 
-        plt.xlabel('1=Ikke tilfreds | 5=Svært tilfreds')
-        plt.savefig(f'{RESULTS_DIR}/satisfaction/{title.replace(" ", "_").lower()}_bar.png')
+def plot_satisfaction(*columns):
+    """Questions related to scale of 1=Ikke tilfreds to 5=Svært tilfreds"""
+    for column in columns:
+        plt, title = plot_diagram(column, 1, 5 + 1)
+
+        plt.xlabel("1=Ikke tilfreds | 5=Svært tilfreds")
+        plt.savefig(
+            f'{RESULTS_DIR}/satisfaction/{title.replace(" ", "_").lower()}_bar.png'
+        )
         plt.close()
 
 
 def plot_agreement(*columns):
-    """ Questions related to scale of 1=Helt uenig to 5=Helt enig """
+    """Questions related to scale of 1=Helt uenig to 5=Helt enig"""
     for column in columns:
-        plt, title = plot_diagram(column, 1, 5+1)
+        plt, title = plot_diagram(column, 1, 5 + 1)
 
-        plt.xlabel('1=Helt uenig | 5=Helt enig')
-        plt.savefig(f'{RESULTS_DIR}/agreement/{title.replace(" ", "_").lower()}_bar.png')
+        plt.xlabel("1=Helt uenig | 5=Helt enig")
+        plt.savefig(
+            f'{RESULTS_DIR}/agreement/{title.replace(" ", "_").lower()}_bar.png'
+        )
         plt.close()
 
 
@@ -71,27 +76,27 @@ def plot_diagram(column, x_low, x_high):
 
     bar_values = [values.count(i) for i in range(x_low, x_high)]
     bars = np.arange(x_low, x_high)
-    
-    mean = round(sum(values) / len(values), 1)
-    plt.bar(bars, bar_values, align='center')
 
-    plt.title(f'{format_title(title)}\nGjennomsnitt: {str(mean)}')
+    mean = round(sum(values) / len(values), 1)
+    plt.bar(bars, bar_values, align="center")
+
+    plt.title(f"{format_title(title)}\nGjennomsnitt: {str(mean)}")
     plt.subplots_adjust(top=0.8)
-    plt.grid(axis='y')
-    plt.ylabel('Antall svar')
+    plt.grid(axis="y")
+    plt.ylabel("Antall svar")
 
     return plt, title
 
 
-if __name__ == '__main__':
-     
+if __name__ == "__main__":
+
     # Questions about Hours spent
     # columns_for_hours = ['D', 'E']
     # plot_hours_spent(*columns_for_hours)
-    
+
     # Questions about Satisfaction
     # columns_for_satisfaction = ['F', 'G']
-    columns_for_satisfaction = ['BK']
+    columns_for_satisfaction = ["BK"]
     plot_satisfaction(*columns_for_satisfaction)
 
     # Questions about Agreement
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Feedback analysis with matplotlib
+
+This is a special purpose script to generate diagrams to reflect the feedback recieved from the
+students taking the course TDT4140 at NTNU.
